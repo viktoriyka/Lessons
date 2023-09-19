532d60284f2c856fb89a8be3db721ce07e7b4f50
viktoriyka <viktoriiakliponosova@gmail.com>
2023-09-20 02:18:55 +0300

    Homework 1

--- src/Homeworks/HW2/Main.java
+++ src/Homeworks/HW2/Main.java
@@ -1,4 +1,13 @@
 package Homeworks.HW2;
 
 public class Main {
+    public static void main(String[] args) {
+
+        float[] nums = new float[]{3.43f, 12.1212f, 12.22f, 2.29f};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + "; ");
+        }
+
+    }
 }
