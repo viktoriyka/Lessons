776ea21724a2b969396b64f632a7ecd446a7f9ff
viktoriyka <viktoriiakliponosova@gmail.com>
2023-10-25 17:15:14 +0300

    Homework 6 (Lesson 7) updated

--- src/Homeworks/HW6/Circle.java
+++ src/Homeworks/HW6/Circle.java
@@ -1,20 +1,3 @@
 package Homeworks.HW6;
 
-public class Circle extends Figure {
-    private int radius;
-
-    public Circle(int radius) {
-        this.radius = radius;
-    }
-
-    @Override
-    public double area() {
-        return Math.PI * radius * radius;
-    }
-
-    @Override
-    public double perimeter() {
-        return 2 * Math.PI * radius;
-    }
-}
 
