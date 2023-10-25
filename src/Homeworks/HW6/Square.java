78dcdf67b315e21005dd48d1ecc19f6b5857e108
viktoriyka <viktoriiakliponosova@gmail.com>
2023-10-25 17:18:14 +0300

    Homework 6 (Lesson 7) updated

--- src/Homeworks/HW6/Square.java
+++ src/Homeworks/HW6/Square.java
@@ -1,17 +1,18 @@
 package Homeworks.HW6;
 
 public class Square extends Figure{
-private int aSideLength;
+    private int aSideLength;
 
     public Square(int aSideLength) {
         this.aSideLength = aSideLength;
     }
 
+    @Override
     public double area() {
         return aSideLength * aSideLength;
     }
 
-
+    @Override
     public double perimeter() {
         return aSideLength * 4;
     }
